2e9fbb76b591cb6e9b680c00b49586a5d6c82cd4
Nosson M Frankel <nossonmfrankel@gmail.com>
2022-08-15 09:44:19 +0300

    Should fix Vercel routing

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -8,21 +8,11 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter({
-			out: 'build',
-			fallback: null, //'200.html'
-			precompress: false
-		}),
-
-		prerender: {
-			// This can be false if you're using a fallback (i.e. SPA mode)
-			default: true
-		},
-
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
+		adapter: adapter({ out: 'build' })
+	},
+	prerender: {
+		// This can be false if you're using a fallback (i.e. SPA mode)
+		default: true
 	}
 }
 
